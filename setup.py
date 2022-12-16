3824f0d0f344e1943b1d84d5ba9c66b0ff8720bb
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-16 15:20:12 +0100

    feat: Create first version of `hello` package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='hello',
+   version='1.0.0',
+   description='Hello World Example',
+   author='Ondrej Sika',
+   author_email='ondrej@ondrejsika.com',
+   packages=['hello'],
+   install_requires=[],
+)
